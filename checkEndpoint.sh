0fcaf94f219f0d4eafd0e7ed1dafb3c3af72c977
bdasilvaol <bruno.oliveira2@engdb.com.br>
2021-08-18 17:29:18 -0300

    Correções

--- checkEndpoint.sh
+++ checkEndpoint.sh
@@ -2,33 +2,56 @@
 #
 # checkEndpoint.sh - Checa o host e port do serviço.
 #
-# Como usar: ". checkEndpoint.sh ambiente nome-do-pod-v1" para checar o host e port no ambiente.
-#
-# Exemplo:
-#       felipe_alencar@cloudshell:~$ . checkEndpoint.sh u-billing-profile-info-v1 uat3
-#
-# Histórico de versões:
-#       Versão: 1.0
-#             Autor: Felipe de Carvalho Alencar <felipe.alencar@engdb.com.br>
-#             Data: 01/2021
-#             Descrição: Primeira versão.
-#       Versão: 2.0
-#             Autor: Heitor Bellini <heitor.bellini@engdb.com.br>
-#             Data: 04/2021
-#             Descrição: Nome do pod não é mais case sensitive. Agora traz o apontamento de db adapters. Namespace referenciada ao namespace.txt.
+#Baseado no trabalho de Felipe de Carvalho Alencar <felipe.alencar@engdb.com.br> e Heitor Bellini <heitor.bellini@engdb.com.br>
+#Migrado para gitlab e alterações feitas por Bruno Oliveira <bruno.oliveira2@engdb.com.br>
 
-if [ -z $2 ] 
-then
-	read -r np<namespace.txt
-else
-np=$2
+svc=$(echo $1 | sed -r 's/[A-Z]/\L&/g')
+if [ -z $svc ]; then
+	svc=help
 fi
-svc=`echo $1 | sed -r 's/[A-Z]/\L&/g'`
+	case $svc in
+		help)
+			echo "Uso checkEndpoint.sh serviço ambiente"
+			echo "Ex: checkEndpoint.sh r-access-info uat"
+			exit 1
+	esac
+
+if [ -z $2 ]; then
+	if [ -f namespace.txt ]; then
+		read -r namespace<namespace.txt
+	else
+		echo "Ambiente nao informado e arquivo namespace.txt nao existe"
+		exit 1
+	fi
+else
+	namespace=$(echo "$2" | awk '{print tolower($0)}')
+	case $namespace in
+		uta|uta2|uta3)
+			namespace=$(sed 's/uta/uat/g' <<< $namespace)
+			;;
+		tua|tua2|tua3)
+			namespace=$(sed 's/tua/uat/g' <<< $namespace)
+			;;
+		aut|aut2|aut3)
+			namespace=$(sed 's/aut/uat/g' <<< $namespace)
+			;;
+		uta1|tua1|aut1)
+			namespace=$(sed -e 's/uta1/uat/g' -e 's/aut1/uat/g' -e 's/tua1/uat/g' <<< $namespace)
+			;;
+		uat|uat2|uat3|prd|dev)
+			echo "Um momento ..."
+			;;
+		*)
+			echo "Ambiente nao informado - Ex: uat, uat2, prd etc ..."
+			echo "Ex: checkEndpoint.sh r-access-info uat"
+			exit 1 
+	esac
+	fi
 
 checkEndpoint (){
-if [ "`kubectl -n $np describe deploy $svc | grep -i db_host`" != "" ]; then
+if [ "`kubectl -n $namespace describe deploy $svc | grep -i db_host`" != "" ]; then
 
-        kubectl -n $np describe deploy $svc > dbadapter.temp
+        kubectl -n $namespace describe deploy $svc > dbadapter.temp
         user=`cat dbadapter.temp | grep -i user_db | cut -d : -f 2 | sed 's/^ *//g'`
         password=`cat dbadapter.temp | grep -i password_db | cut -d : -f 2 | sed 's/^ *//g'`
         host=`cat dbadapter.temp | grep -i db_host | cut -d : -f 2 | sed 's/^ *//g'`
@@ -36,24 +59,14 @@ if [ "`kubectl -n $np describe deploy $svc | grep -i db_host`" != "" ]; then
         schema=`cat dbadapter.temp | grep -i schema | cut -d : -f 2 | sed 's/^ *//g'`
         echo "PROVIDER_ADDRESS:      $user/$password@$host:$port/$schema"
 
-        mapper=`kubectl exec -it deploy/$svc -n $np -- bash -c "grep -i mapper resources/*.bpmn" | cut -d \" -f 4 | sed 's/^ *//g'`
+        mapper=`kubectl exec -it deploy/$svc -n $namespace -- bash -c "grep -i mapper resources/*.bpmn" | cut -d \" -f 4 | sed 's/^ *//g'`
 
-        proc=`kubectl exec -it deploy/$svc -n $np -- bash -c "grep -i call resources/mappers/$mapper.xml" | grep -i -v callable | sed 's/^ *//g' | cut -d \( -f 1 | cut -d ' ' -f 2`
+        proc=`kubectl exec -it deploy/$svc -n $namespace -- bash -c "grep -i call resources/mappers/$mapper-$namespace.xml" | grep -i -v callable | sed 's/^ *//g' | cut -d \( -f 1 | cut -d ' ' -f 2`
         echo "PROCEDURE:             $proc"
 
 else
-        kubectl -n $np describe deploy $svc | grep -i provider_address | sed 's/^ *//g'
+        kubectl -n $namespace describe deploy $svc | grep -i provider_address | sed 's/^ *//g'
 fi
 }
 
-checkEndpoint $svc $np
-
-##echo "$?"
-##if [ $? -eq 0 ]
-#then
- #       echo ""
-#else
-#        echo ""
-#		echo "Utilize o script conforme o exemplo: . checkEndpoint.sh u-billing-profile-info-v1 uat3"
-#		echo ""
-#fi
+checkEndpoint $svc $namespace
