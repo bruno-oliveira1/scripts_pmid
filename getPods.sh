4090825c2af6f7bbd57f9c1ab4c798cb00f51294
bdasilvaol <bruno.oliveira2@engdb.com.br>
2021-09-08 16:42:17 -0300

    Organização

--- getPods.sh
+++ getPods.sh
@@ -6,7 +6,14 @@
 #Baseado no trabalho de Heitor Bellini <heitor.bellini@engdb.com.br> e Flavio Moreira <flavio.moreira@engdb.com.br>
 #Migrado para gitlab e alterações feitas por Bruno Oliveira <bruno.oliveira2@engdb.com.br>
 
-if [ -z $2 ]; then
+getPods (){
+kubectl get pods -A | grep $namespace 2>/dev/null | grep $pod
+}
+
+pod=$(echo "$1" | awk '{print tolower($0)}')
+namespace=$(echo "$2" | awk '{print tolower($0)}')
+
+if [ -z $namespace ]; then
 	if [ -f namespace.txt ]; then
 		read -r namespace<namespace.txt		
 	else
@@ -14,32 +21,27 @@ if [ -z $2 ]; then
 	exit 1
 	fi
 else
-	namespace=$(echo "$2" | awk '{print tolower($0)}')
 	case $namespace in
 		uta|uta2|uta3) 
 			namespace=$(sed 's/uta/uat/g' <<< $namespace)
+			getPods $pod $namespace
 			;;
 		tua|tua2|tua3) 
 			namespace=$(sed 's/tua/uat/g' <<< $namespace)
+			getPods $pod $namespace
 			;;
 		aut|aut2|aut3) 
 			namespace=$(sed 's/aut/uat/g' <<< $namespace)
+			getPods $pod $namespace
 			;;
 		uat1|uta1|tua1|aut1)
 			namespace=$(sed -e 's/uat1/uat/' -e 's/uta1/uat/g' -e 's/aut1/uat/g' -e 's/tua1/uat/g' <<< $namespace)
+			getPods $pod $namespace
 			;;
 		uat|uat2|uat3|prd|dev)
-			echo "Um momento ..."
+			getPods $pod $namespace
 			;;
 		*)
-			echo "Ambiente nao informado - Ex: uat, uat2, prd etc ..." 
+			echo "Ambiente informado diferente do esperado - Ex: uat, uat2, prd etc ..." 
 	esac
 fi
-
-pod=$(echo "$1" | awk '{print tolower($0)}')
-
-getPods (){
-kubectl get pods -A | grep $namespace 2>/dev/null | grep $pod
-}
-
-getPods $pod $namespace
