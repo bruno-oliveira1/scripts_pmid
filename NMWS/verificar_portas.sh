ef313a7330eb68c8e4b6ab497e32bc2840563e70
Bruno da Silva Oliveira <bruno.oliveira2@engdb.com.br>
2021-11-23 16:53:50 -0300

    Script para testar conectividade

--- NMWS/verificar_portas.sh
+++ NMWS/verificar_portas.sh
@@ -2,25 +2,29 @@
 #Fonte https://stackoverflow.com/questions/23421917/bash-script-telnet-to-test-multiple-addresses-and-ports
 #Quem copiou e traduziu as mensagens de respostas e adaptou Bruno Oliveira - bruno.oliveira@engdb.com.br
 #Le o arquivo firewall.txt e com base nisso testa conectividade
-#Formato esperado pelo firewall.txt host porta um por linha 
+#Formato esperado pelo firewall.txt host porta um por linha
 #Ex:
 # google.com.br 443
 # facebook.com 443
 
-cat firewall.txt | (
+#echo "scanme.nmap.org 80
+#scanme.nmap.org 81
+#192.168.0.100 1" | (
+
+ cat firewall.txt | (
   TCP_TIMEOUT=3
   while read host port; do
-    (CURPID=87;
-    (sleep ;kill ) &
-    exec 3<> /dev/tcp//
+    (CURPID=$BASHPID;
+    (sleep $TCP_TIMEOUT;kill $CURPID) &
+    exec 3<> /dev/tcp/$host/$port
     ) 2>/dev/null
-    case 0 in
+    case $? in
     0)
-      echo -e "  \t[32mAberta(B[0m";;
+      echo -e "${host} ${port} Aberta";;
     1)
-      echo -e "  \t[31mFechada(B[0m";;
+      echo -e "$host $port Fechada";;
     143) # killed by SIGTERM
-      echo -e "  \t[33mTimeout(B[0m";;
+       echo -e "$host $port Timeout";;
      esac
   done
-  ) 2>/dev/null # avoid bash message "Terminated ..."
+  ) 2>/dev/null
