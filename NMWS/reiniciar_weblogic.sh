c4b5090872db7ab58f134200fe7310ddbcdec24a
Bruno da Silva Oliveira <bruno.oliveira2@engdb.com.br>
2021-11-23 17:04:41 -0300

    Script restart wli

---
+++ NMWS/reiniciar_weblogic.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+user=$(whoami)
+
+if [ $user == faq1 ]; then
+    #WLI UAT1
+    ps -ef | grep java | grep fqa1 | grep -v grep | awk -F' ' '{ print $2 }'
+    while [ $? -eq 0 ]; do
+        kill -9 $(ps -ef | grep java | grep fqa1 | grep -v grep | awk -F' ' '{ print $2 }')
+    done
+    cd /soa/fqa1/user_projects/domains/IB_FQA1_DOM && ./startNodeManager.sh && ./startWebLogic.sh
+elif [ $user == uat2 ]; then
+    #WLI UAT2
+    ps -ef | grep java | grep uat2 | grep -v grep | awk -F' ' '{ print $2 }'
+    while [ $? -eq 0 ]; do
+        kill -9 $(ps -ef | grep java | grep uat2 | grep -v grep | awk -F' ' '{ print $2 }')
+    done
+    cd /appl/uat2/domains/wli_uat2_domain && ./startNodeManager.sh && ./startWebLogic.sh
+else
+    echo "Nao host não é uat1 ou uat2"
+fi
