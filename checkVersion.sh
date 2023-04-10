9559d7af7d45974112388e7e72cab6b4ea3e8f42
bdasilvaol <bruno.oliveira2@engdb.com.br>
2023-04-10 18:11:29 -0300

    Add: Retornar versão de serviço gmid

--- checkVersion.sh
+++ checkVersion.sh
@@ -5,29 +5,51 @@
 #Baseado no trabalho de Felipe de Carvalho Alencar <felipe.alencar@engdb.com.br>
 #Migrado para gitlab e alterações feitas por Bruno Oliveira <bruno.oliveira2@engdb.com.br>
 
+token=$(cat $HOME/token 2>/dev/null)
+if [[ $token == "" ]]; then
+  echo "Eh necessario ter um token criado no GitLab que o mesmo esteja dentro do arquivo $HOME/token"
+  echo "Para que esse script funcione"
+  echo 'Ex: cat $HOME/token'
+  echo 'pqamgereorppreq'
+  exit 1
+fi
+
+tokenfull="Authorization: Bearer $token"
+
 if [ -z $2 ]; then
-	if [ -f $HOME/namespace.txt ]; then
-		read -r namespace<$HOME/namespace.txt
-	else
-		echo "Ambiente nao informado e arquivo namespace.txt nao existe"
-		exit 1
-	fi
+  if [ -f $HOME/namespace.txt ]; then
+    read -r namespace <$HOME/namespace.txt
+  else
+    echo "Ambiente nao informado e arquivo namespace.txt nao existe"
+    exit 1
+  fi
 else
-	namespace=$(echo "$2" | awk '{print tolower($0)}')
-	fi
+  namespace=$(echo "$2" | awk '{print tolower($0)}')
+fi
 
 pod=$(echo "$1" | awk '{print tolower($0)}')
-checkVersion (){
-	kubectl -n $namespace describe deploy $pod 2>/dev/null | grep -i Image || kubectl -n m-$namespace describe deploy $pod 2>/dev/null | grep -i Image || kubectl -n s-$namespace describe deploy $pod 2>/dev/null | grep -i Image || kubectl -n e-$namespace describe deploy $pod 2>/dev/null | grep -i Image || versao=naoinstalado
+checkVersion() {
+  kubectl -n $namespace describe deploy $pod 2>/dev/null | grep -i Image || kubectl -n m-$namespace describe deploy $pod 2>/dev/null | grep -i Image || kubectl -n s-$namespace describe deploy $pod 2>/dev/null | grep -i Image || kubectl -n e-$namespace describe deploy $pod 2>/dev/null | grep -i Image || versao=naoinstalado
 }
 
-checkVersion > /dev/null
+if echo "$pod" | grep -q "gmid-"; then
+  tipo="gmid/services"
+  servico=${pod/gmid-/}
+  versao=$(checkVersion | sed 's/.*pipeline-//')
+  id=$(curl -s --header "$tokenfull" -X GET https://gitlab.engdb.com.br/api/v4/projects?search=$servico | jq --arg tipo "$tipo" --arg servico "$servico" '.[] | select(.namespace.full_path == $tipo and .name == $servico) | .id')
+  versao=$(curl -s --header "$tokenfull" -X GET https://gitlab.engdb.com.br/api/v4/projects/$id/pipelines?per_page=300 | jq --arg versao "$versao" '.[] | select(.id == ($versao | tonumber)) | .ref' --raw-output)
+  echo -e "$pod $versao $namespace"
+  exit 0
+else
+  checkVersion >/dev/null
+fi
 
 case "$versao" in
-        naoinstalado)
-		echo -e "Nao instalado"
-        ;;
-        *)
-        versao=$(checkVersion $pod $namespace  | awk -F : '{print $3}')
-		echo -e "$pod $versao $ambiente"
+naoinstalado)
+  echo -e "Nao instalado"
+  ;;
+*)
+  versao=$(checkVersion $pod $namespace | awk -F : '{print $3}')
+  echo -e "$pod $versao $ambiente"
+  ;;
 esac
