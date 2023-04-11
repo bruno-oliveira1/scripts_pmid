4126beb5b9c851b1009090cf87e13696037e5883
bdasilvaol <bruno.oliveira2@engdb.com.br>
2023-04-11 17:37:15 -0300

    Add: retornando pipeline para rules

--- checkVersion.sh
+++ checkVersion.sh
@@ -19,6 +19,7 @@ tokenfull="Authorization: Bearer $token"
 if [ -z $2 ]; then
   if [ -f $HOME/namespace.txt ]; then
     read -r namespace <$HOME/namespace.txt
+    echo $namespace
   else
     echo "Ambiente nao informado e arquivo namespace.txt nao existe"
     exit 1
@@ -28,29 +29,41 @@ else
 fi
 
 pod=$(echo "$1" | awk '{print tolower($0)}')
-checkVersion() {
-  kubectl -n $namespace describe deploy $pod 2>/dev/null | grep -i Image || kubectl -n m-$namespace describe deploy $pod 2>/dev/null | grep -i Image || kubectl -n s-$namespace describe deploy $pod 2>/dev/null | grep -i Image || kubectl -n e-$namespace describe deploy $pod 2>/dev/null | grep -i Image || versao=naoinstalado
-}
+na_namespace="$namespace"
 
 if echo "$pod" | grep -q "gmid-"; then
   tipo="gmid/services"
   servico=${pod/gmid-/}
-  versao=$(checkVersion | sed 's/.*pipeline-//')
+  namespace="m-$na_namespace"
+  versao=$(kubectl -n $namespace describe deploy $pod 2>/dev/null | grep -i Image | sed 's/.*pipeline-//')
+  if [[ $versao != "naoinstalado" && $versao != "" ]]; then
+    id=$(curl -s --header "$tokenfull" -X GET https://gitlab.engdb.com.br/api/v4/projects?search=$servico | jq --arg tipo "$tipo" --arg servico "$servico" '.[] | select(.namespace.full_path == $tipo and .name == $servico) | .id')
+    versao=$(curl -s --header "$tokenfull" -X GET https://gitlab.engdb.com.br/api/v4/projects/$id/pipelines?per_page=300 | jq --arg versao "$versao" '.[] | select(.id == ($versao | tonumber)) | .ref' --raw-output)
+    pipeline=$(curl -s --header "$tokenfull" -X GET https://gitlab.engdb.com.br/api/v4/projects/$id/pipelines?per_page=300 | jq --arg versao "$versao" '.[] | select(.ref == $versao) | .id')
+    echo -e "$pod $namespace $versao pipeline: https://gitlab.engdb.com.br/$tipo/$servico/-/pipelines/$pipeline"
+    exit 0
+  else
+    echo -e "Nao instalado"
+  fi
+elif echo "$pod" | grep -q "rules-"; then
+  servico="$pod"
+  tipo="pmid/brms"
+  namespace="s-$na_namespace"
+  versao=$(kubectl -n $namespace describe deploy $pod 2>/dev/null | grep -i Image | sed 's#.*:##')
   id=$(curl -s --header "$tokenfull" -X GET https://gitlab.engdb.com.br/api/v4/projects?search=$servico | jq --arg tipo "$tipo" --arg servico "$servico" '.[] | select(.namespace.full_path == $tipo and .name == $servico) | .id')
-  versao=$(curl -s --header "$tokenfull" -X GET https://gitlab.engdb.com.br/api/v4/projects/$id/pipelines?per_page=300 | jq --arg versao "$versao" '.[] | select(.id == ($versao | tonumber)) | .ref' --raw-output)
-  pipeline=$(curl -s --header "$tokenfull" -X GET https://gitlab.engdb.com.br/api/v4/projects/$id/pipelines?per_page=300 | jq --arg versao "$versao" '.[] | select(.ref == $versao) | .id')
-  echo -e "$pod $namespace $versao pipeline: https://gitlab.engdb.com.br/$tipo/$servico/-/pipelines/$pipeline"
-  exit 0
+  if [[ $versao != "naoinstalado" && $versao != "" ]]; then
+    pipeline=$(curl -s --header "$tokenfull" -X GET https://gitlab.engdb.com.br/api/v4/projects/$id/pipelines?per_page=300 | jq --arg versao "$versao" '.[] | select(.ref == $versao) | .id')
+    declare -g namespace="s-$namespace"
+    echo -e "$pod $namespace $versao pipeline: https://gitlab.engdb.com.br/$tipo/$servico/-/pipelines/$pipeline"
+    exit 0
+  else
+    echo -e "Nao instalado"
+  fi
 else
-  checkVersion >/dev/null
+  versao=$(kubectl -n $namespace describe deploy $pod 2>/dev/null | grep -i Image | sed 's#.*:##')
+  if [[ $versao != "naoinstalado" && $versao != "" ]]; then
+    echo -e "$pod $namespace $versao"
+  else
+    echo -e "Nao instalado"
+  fi
 fi
-
-case "$versao" in
-naoinstalado)
-  echo -e "Nao instalado"
-  ;;
-*)
-  versao=$(checkVersion $pod $namespace | awk -F : '{print $3}')
-  echo -e "$pod $versao $ambiente"
-  ;;
-esac
